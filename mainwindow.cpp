c47db3cb6cea237024d9c94d2205a27bd993db38
Harish <harishbappleid@gmail.com>
2020-06-28 19:01:40 -0400

    Captured key press event. Tried translating a few keys into tamil literals. Added a layout and text box.'

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include <QtWidgets/QTextEdit>
+#include <QHBoxLayout>
+#include <QKeyEvent>
 #include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
@@ -9,9 +11,23 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     m_text_editor = new QTextEdit(parent);
-    m_text_editor->setGeometry(10, 10, 80, 30);
-    this->setCentralWidget(m_text_editor);
-    connect(m_text_editor, SIGNAL(textChanged()), this, SLOT(handleTextChange()));
+    m_tamil_editor = new QTextEdit(parent);
+
+    m_layout = new QHBoxLayout();
+
+    m_text_editor->setGeometry(10, 10, 20, 20);
+    m_tamil_editor->setGeometry(10, 30, 20, 20);
+
+    m_layout->addWidget(m_text_editor);
+    m_layout->addWidget(m_tamil_editor);
+
+    this->centralWidget()->setLayout(m_layout);
+
+    m_tamil_editor->setText("தமிழ்");
+    m_tamil_editor->setReadOnly(true);
+    this->setFocus();
+    //m_text_editor->setFocus();
+    //connect(m_text_editor, SIGNAL(textChanged()), this, SLOT(handleTextChange()));
 
 }
 
@@ -20,7 +36,35 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::keyPressEvent(QKeyEvent* event)
+{
+    using namespace Qt;
+    QString tamilKey = "";
+    switch(event->key())
+    {
+        case Key::Key_A:
+            tamilKey = "அ"; break;
+        case Key::Key_Q:
+            tamilKey = "ஆ"; break;
+        case Key::Key_S:
+            tamilKey = "இ"; break;
+        default:
+            break;
+    }
+    QString tamilText = m_tamil_editor->toPlainText();
+    tamilText += tamilKey;
+    m_tamil_editor->setText(tamilText);
+}
+/*
 void MainWindow::handleTextChange()
 {
-    m_text_editor->setText("Hello");
+    QString englishText = m_text_editor->toPlainText();
+    const auto lastCharIndex = englishText.length() - 1;
+    if(englishText.at(lastCharIndex) == 'a')
+    {
+        QString tamilText = m_tamil_editor->toPlainText();
+        tamilText += "அ";
+        m_tamil_editor->setText(tamilText);
+    }
 }
+*/
--- mainwindow.h
+++ mainwindow.h
@@ -8,6 +8,7 @@ namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class QTextEdit;
+class QHBoxLayout;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -16,10 +17,14 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-    void handleTextChange();
+    void keyPressEvent(QKeyEvent* event);
+//public slots:
+//    void handleTextChange();
 
 private:
     QTextEdit *m_text_editor;
+    QTextEdit *m_tamil_editor;
+    QHBoxLayout *m_layout;
     Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
