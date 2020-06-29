0576c887bfb3dd9e4b767021d98f50f54bbfe932
Harish Bala <harishbappleid@gmail.com>
2020-06-29 05:58:37 -0400

    Added key to tamil key translation map and consonant set.

--- QtKeyToTamilKey.cpp
+++ QtKeyToTamilKey.cpp
@@ -1,48 +1,76 @@
 #include <QtKeyToTamilKey.h>
 
+QtKeyToTamilKey::QtKeyToTamilKey()
+{
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_A, TamilKeyMapping("அ", "௹", "அ")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_Q, TamilKeyMapping("ஆ", "ஸ", "\u0BBE")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_S, TamilKeyMapping("இ", "௺", "\u0BBF")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_W, TamilKeyMapping("ஈ", "ஷ", "\u0BC0")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_D, TamilKeyMapping("உ", "௸", "\u0BC1")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_E, TamilKeyMapping("ஊ", "ஜ", "\u0BC2")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_G, TamilKeyMapping("எ", "", "\u0BC6")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_T, TamilKeyMapping("ஏ", "க்ஷ", "\u0BC7")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_R, TamilKeyMapping("ஐ", "ஹ", "\u0BC8")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_C, TamilKeyMapping("ஒ", "௵", "\u0BCA")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_X, TamilKeyMapping("ஓ", "௴", "\u0BCB")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_Z, TamilKeyMapping("ஔ", "௳", "\u0BCC")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_F, TamilKeyMapping("்", "ஃ", "்")));
 
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_H, TamilKeyMapping("க", "௳", "")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_Y, TamilKeyMapping("ள", "௳", "")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_J, TamilKeyMapping("ப", "௳", "")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_U, TamilKeyMapping("ற", "௳", "")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_K, TamilKeyMapping("ம", "௳", "")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_I, TamilKeyMapping("ன", "௳", "")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_L, TamilKeyMapping("த", "௳", "")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_O, TamilKeyMapping("ட", "௳", "")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_P, TamilKeyMapping("ண", "௳", "")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_Semicolon, TamilKeyMapping("ந", "௳", "")));
+    m_key_map.insert(std::pair<int, TamilKeyMapping>(Key::Key_BraceLeft, TamilKeyMapping("ச", "௳", "")));
+
+    m_consonant_set.insert("க");
+    m_consonant_set.insert("ச");
+    m_consonant_set.insert("ட");
+    m_consonant_set.insert("த");
+    m_consonant_set.insert("ப");
+    m_consonant_set.insert("ற");
+    m_consonant_set.insert("ய");
+    m_consonant_set.insert("ர");
+    m_consonant_set.insert("ல");
+    m_consonant_set.insert("வ");
+    m_consonant_set.insert("ழ");
+    m_consonant_set.insert("ள");
+    m_consonant_set.insert("ஞ");
+    m_consonant_set.insert("ங");
+    m_consonant_set.insert("ன");
+    m_consonant_set.insert("ந");
+    m_consonant_set.insert("ம");
+    m_consonant_set.insert("ண");
+}
 const QString QtKeyToTamilKey::getTamilKey(int key)
 {
     QString tamilKey = "";
-    switch(key)
-    {
-        case Key::Key_A:
-            tamilKey = "அ"; break;
-        case Key::Key_Q:
-            tamilKey = "ஆ"; break;
-        case Key::Key_S:
-            tamilKey = "இ"; break;
-        case Key::Key_H:
-            tamilKey = "க"; break;
-        case Key::Key_Space:
-            tamilKey = " "; break;
-        default:
-            break;
-    }
-
-    if(key == Key::Key_S)
+    auto key_element = m_key_map.find(key);
+    if(key_element != m_key_map.end())
     {
         if(isPrevKeyTamilConsonant())
         {
-            tamilKey = "\u0BBF";
+            tamilKey = key_element->second.next_to_cons_press;
         }
         else
         {
-            tamilKey = "இ";
+            tamilKey = key_element->second.simple_press;;
         }
     }
-    m_prev_key = tamilKey;
 
+    if(tamilKey != "")
+    {
+        m_prev_key = tamilKey;
+    }
     return tamilKey;
 }
 
 bool QtKeyToTamilKey::isPrevKeyTamilConsonant()
 {
-    bool isConsonant = false;
-    if(m_prev_key == "க")
-    {
-        // a lot more consonants to be added
-        isConsonant = true;
-    }
-    return isConsonant;
+    return (m_consonant_set.find(m_prev_key) != m_consonant_set.end());
 }
--- QtKeyToTamilKey.h
+++ QtKeyToTamilKey.h
@@ -1,16 +1,41 @@
 #ifndef QTKEYTOTAMILKEY_H
 #define QTKEYTOTAMILKEY_H
+#include <map>
+#include <set>
 #include <QKeyEvent>
 
 using namespace Qt;
 
+class TamilKeyMapping
+{
+public:
+    TamilKeyMapping() :
+        simple_press(""),
+        shift_press(""),
+        next_to_cons_press("")
+    {}
+
+    TamilKeyMapping(QString param1, QString param2, QString param3) :
+        simple_press(param1),
+        shift_press(param2),
+        next_to_cons_press(param3)
+    {}
+
+    QString simple_press;
+    QString shift_press;
+    QString next_to_cons_press;
+};
+
 class QtKeyToTamilKey
 {
 public:
+    QtKeyToTamilKey();
     const QString getTamilKey(int key);
 private:
     bool isPrevKeyTamilConsonant();
     QString m_prev_key = "";
+    std::map<int, TamilKeyMapping> m_key_map;
+    std::set<QString> m_consonant_set;
 };
 
 #endif // QTKEYTOTAMILKEY_H
