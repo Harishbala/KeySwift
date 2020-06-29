6a421cb3c6dcb521222a91e3889944ca399e8ff5
Harish Bala <harishbappleid@gmail.com>
2020-06-29 10:12:46 -0400

    Added few tamil key in translation table

--- QtKeyToTamilKey.cpp
+++ QtKeyToTamilKey.cpp
@@ -3,18 +3,20 @@
 QtKeyToTamilKey::QtKeyToTamilKey()
 {
 
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("a", TamilKeyMapping("அ", "அ")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("q", TamilKeyMapping("ஆ", "\u0BBE")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("s", TamilKeyMapping("இ", "\u0BBF")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("w", TamilKeyMapping("ஈ", "\u0BC0")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("d", TamilKeyMapping("உ", "\u0BC1")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("e", TamilKeyMapping("ஊ", "\u0BC2")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("g", TamilKeyMapping("எ", "\u0BC6")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("t", TamilKeyMapping("ஏ", "\u0BC7")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("r", TamilKeyMapping("ஐ", "\u0BC8")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("c", TamilKeyMapping("ஒ", "\u0BCA")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("x", TamilKeyMapping("ஓ", "\u0BCB")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("z", TamilKeyMapping("ஔ", "\u0BCC")));
+    //Tamil unicode block: https://en.wikipedia.org/wiki/Tamil_(Unicode_block)
+
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("a", TamilKeyMapping("அ", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("q", TamilKeyMapping("ஆ", "ா")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("s", TamilKeyMapping("இ", "ி")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("w", TamilKeyMapping("ஈ", "ீ")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("d", TamilKeyMapping("உ", "ு")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("e", TamilKeyMapping("ஊ", "ூ")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("g", TamilKeyMapping("எ", "ெ")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("t", TamilKeyMapping("ஏ", "ே")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("r", TamilKeyMapping("ஐ", "ை")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("c", TamilKeyMapping("ஒ", "ொ")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("x", TamilKeyMapping("ஓ", "ோ")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("z", TamilKeyMapping("ஔ", "ௌ")));
     m_key_map.insert(std::pair<QString, TamilKeyMapping>("f", TamilKeyMapping("்", "்")));
 
     m_key_map.insert(std::pair<QString, TamilKeyMapping>("h", TamilKeyMapping("க", "")));
@@ -37,33 +39,39 @@ QtKeyToTamilKey::QtKeyToTamilKey()
     m_key_map.insert(std::pair<QString, TamilKeyMapping>("m", TamilKeyMapping("ர", "")));
     m_key_map.insert(std::pair<QString, TamilKeyMapping>("/", TamilKeyMapping("ழ", "")));
 
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("A", TamilKeyMapping("௹", "அ")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("Q", TamilKeyMapping("ஸ", "\u0BBE")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("S", TamilKeyMapping("௺", "\u0BBF")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("W", TamilKeyMapping("ஷ", "\u0BC0")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("D", TamilKeyMapping("௸", "\u0BC1")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("E", TamilKeyMapping("ஜ", "\u0BC2")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("G", TamilKeyMapping("", "\u0BC6")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("T", TamilKeyMapping("க்ஷ", "\u0BC7")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("R", TamilKeyMapping("ஹ", "\u0BC8")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("C", TamilKeyMapping("௵", "\u0BCA")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("X", TamilKeyMapping("௴", "\u0BCB")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("Z", TamilKeyMapping("௳", "\u0BCC")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("F", TamilKeyMapping("", "்")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("A", TamilKeyMapping("௹", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("Q", TamilKeyMapping("ஸ", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("S", TamilKeyMapping("௺", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("W", TamilKeyMapping("ஷ", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("D", TamilKeyMapping("௸", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("E", TamilKeyMapping("ஜ", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("G", TamilKeyMapping("", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("T", TamilKeyMapping("க்ஷ", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("R", TamilKeyMapping("ஹ", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("C", TamilKeyMapping("௵", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("X", TamilKeyMapping("௴", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("Z", TamilKeyMapping("௳", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("F", TamilKeyMapping("ஃ", "")));
 
     m_key_map.insert(std::pair<QString, TamilKeyMapping>("H", TamilKeyMapping("", "")));
     m_key_map.insert(std::pair<QString, TamilKeyMapping>("Y", TamilKeyMapping("ஸ்ரீ", "")));
     m_key_map.insert(std::pair<QString, TamilKeyMapping>("J", TamilKeyMapping("", "")));
     m_key_map.insert(std::pair<QString, TamilKeyMapping>("U", TamilKeyMapping("", "")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("K", TamilKeyMapping("", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("K", TamilKeyMapping("\"", "")));
     m_key_map.insert(std::pair<QString, TamilKeyMapping>("I", TamilKeyMapping("", "")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("L", TamilKeyMapping("", "")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("O", TamilKeyMapping("", "")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("P", TamilKeyMapping("", "")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>(":", TamilKeyMapping("", "")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("{", TamilKeyMapping("", "")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("}", TamilKeyMapping("", "")));
-    m_key_map.insert(std::pair<QString, TamilKeyMapping>("\"", TamilKeyMapping("'", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("L", TamilKeyMapping(":", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("O", TamilKeyMapping("[", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("P", TamilKeyMapping("]", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>(":", TamilKeyMapping(";", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("{", TamilKeyMapping("{", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("}", TamilKeyMapping("}", "")));
+
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("V", TamilKeyMapping("௶", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("B", TamilKeyMapping("௷", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("N", TamilKeyMapping("", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("M", TamilKeyMapping("/", "")));
+    m_key_map.insert(std::pair<QString, TamilKeyMapping>("/", TamilKeyMapping("ழ", "")));
+
     m_vowel_set.insert("அ");
     m_vowel_set.insert("ஆ");
     m_vowel_set.insert("இ");
@@ -95,6 +103,11 @@ QtKeyToTamilKey::QtKeyToTamilKey()
     m_consonant_set.insert("ந");
     m_consonant_set.insert("ம");
     m_consonant_set.insert("ண");
+    m_consonant_set.insert("ஸ");
+    m_consonant_set.insert("ஷ");
+    m_consonant_set.insert("ஜ");
+    m_consonant_set.insert("ஹ");
+    m_consonant_set.insert("க்ஷ");
 }
 const QString QtKeyToTamilKey::getTamilKey(QKeyEvent* event)
 {
