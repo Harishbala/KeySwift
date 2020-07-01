0133913e79b584d48b23bde3797fee9f34d5c7c8
Harish Bala <harishbappleid@gmail.com>
2020-07-01 10:17:29 -0400

    Removed all the dynamically created UI controls in the code. Added all UI controls as part of mainwindow.ui.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,40 +10,19 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-    m_tamil_editor = new TTWTextEdit(parent);
-    m_tamil_editor->installEventFilter(this);
-
-    m_layout = new QHBoxLayout();
-    m_layout->addWidget(m_tamil_editor);
-    m_layout->addWidget(ui->toggleButton);
-
-    this->centralWidget()->setLayout(m_layout);
-
+    ui->textEdit->installEventFilter(this);
 
     QObject::connect(ui->toggleButton, SIGNAL(clicked()), this, SLOT(button_clicked()));
 }
 
 MainWindow::~MainWindow()
 {
-    delete m_tamil_editor;
-    delete m_layout;
     delete ui;
 }
 
 void MainWindow::button_clicked()
 {
-    m_tamil_editor->set_english_toggle(!m_tamil_editor->get_english_toggle());
-}
-
-bool TTWTextEdit::get_english_toggle() const
-{
-    return m_english_toggle;
-}
-
-void TTWTextEdit::set_english_toggle(bool toggle)
-{
-    m_english_toggle = toggle;
+    m_english_toggle = !m_english_toggle;
 }
 
 bool MainWindow::eventFilter(QObject* obj, QEvent* event)
@@ -51,36 +30,36 @@ bool MainWindow::eventFilter(QObject* obj, QEvent* event)
     if(event->type() == QEvent::KeyPress)
     {
         QKeyEvent* key_event = static_cast<QKeyEvent*>(event);
-        if(ownkeyPressEvent(key_event))
+        if(textEditKeyPressed(key_event))
             return true;
     }
     return QObject::eventFilter(obj, event);
 }
 
-bool MainWindow::ownkeyPressEvent(QKeyEvent* event)
+bool MainWindow::textEditKeyPressed(QKeyEvent* event)
 {
-    if(m_tamil_editor->get_english_toggle())
+    if(m_english_toggle)
     {
         return false;
     }
 
     else if(event->key() == Qt::Key::Key_Control)
     {
-        QString tamilText = m_tamil_editor->toPlainText();
+        QString tamilText = ui->textEdit->toPlainText();
         QClipboard *clipboard = QApplication::clipboard();
         clipboard->setText(tamilText);
         return true;
     }
     else if(event->key() == Qt::Key::Key_Alt)
     {
-        m_tamil_editor->clear();
+        ui->textEdit->clear();
         return true;
     }
     else
     {
-        QString tamilText = m_tamil_editor->toPlainText();
+        QString tamilText = ui->textEdit->toPlainText();
         QString prevKey = "";
-        const auto position = m_tamil_editor->textCursor().position();
+        const auto position = ui->textEdit->textCursor().position();
         if(position > 0)
         {
             prevKey = QString(tamilText[position - 1]);
@@ -88,10 +67,9 @@ bool MainWindow::ownkeyPressEvent(QKeyEvent* event)
         QString tamilKey{""};
         if(m_key_translator.getTamilKey(event, prevKey, tamilKey))
         {
-            m_tamil_editor->textCursor().insertText(tamilKey);
+            ui->textEdit->textCursor().insertText(tamilKey);
             return true;
         }
-
     }
 
     return false;
--- mainwindow.h
+++ mainwindow.h
@@ -2,35 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QtWidgets/QTextEdit>
-#include <QPushButton>
 #include <QtKeyToTamilKey.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
-
-class QTextEdit;
-class QHBoxLayout;
-class QPushButton;
-
-class TTWTextEdit : public QTextEdit
-{
-public:
-    TTWTextEdit(QWidget *parent)
-        : QTextEdit(parent),
-          m_english_toggle(false)
-    {}
-
-    void set_english_toggle(bool toggle);
-    bool get_english_toggle() const;
-    void event_handler(QEvent* event);
-//protected:
-    //void keyPressEvent(QKeyEvent* event);
-
-private:
-    bool m_english_toggle;
-};
 
 class MainWindow : public QMainWindow
 {
@@ -47,13 +23,12 @@ protected:
     bool eventFilter(QObject* obj, QEvent* event);
 
 private:
-    bool ownkeyPressEvent(QKeyEvent* event);
+    bool textEditKeyPressed(QKeyEvent* event);
 
     QtKeyToTamilKey key_translator;
-    TTWTextEdit *m_tamil_editor;
-    QHBoxLayout *m_layout;
     QtKeyToTamilKey m_key_translator;
-    //QPushButton toggleButton;
+    bool m_english_toggle;
+
     Ui::MainWindow *ui;
 
 };
